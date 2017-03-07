f5bf2b1bfd32ef04db2fd1f5c0c63ba045e491e1
Puzin Pavel <puzinok@gmail.com>
2017-03-07 11:54:08 +0300

    fix tabs and sqrt duplicate

--- lesson1/quadratic.rb
+++ lesson1/quadratic.rb
@@ -6,14 +6,15 @@ print "Введите c: "
 c = gets.chomp.to_f
 
 d = (b**2) - 4*(a*c)
+sqrt_d = Math.sqrt(d)
 
 if d < 0
-	puts "D = #{d}. Корней нет"
+  puts "D = #{d}. Корней нет"
 elsif d == 0
-	x1x2 = -b/2*a
-	puts "D = 0, X1 = X2 = #{x1x2}"
+  x1x2 = -b/2*a
+  puts "D = 0, X1 = X2 = #{x1x2}"
 else
-	x1 = (-b + Math.sqrt(d))/ (2*a)
-	x2 = (-b - Math.sqrt(d))/ (2*a)
-	puts "D = #{d}, X1 = #{x1}, X2 = #{x2}"
+  x1 = (-b + sqrt_d/ (2*a)
+  x2 = (-b - sqrt_d/ (2*a)
+  puts "D = #{d}, X1 = #{x1}, X2 = #{x2}"
 end
