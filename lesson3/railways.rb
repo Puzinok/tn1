7dfa183a87f0b28b7c84b0bbe722279579fb2b37
Puzin Pavel <puzinok@gmail.com>
2017-03-16 23:39:08 +0300

    Внес исправления

--- lesson3/railways.rb
+++ lesson3/railways.rb
@@ -1,6 +1,6 @@
 class Station
 
-  attr_reader :name
+  attr_reader :name, :trains
 
   def initialize(name)
     @trains = []
@@ -15,18 +15,11 @@ class Station
     @trains.delete(train)
   end
 
-  def all_trains
+  def trains_by_type(type)
+    @type = []
     @trains.each do |t|
-      puts "Поезд: #{t.train[0]}, Тип: #{t.train[1]}, Вагонов: #{t.train[2]}"
+      @type << t if t.type == type
     end
-  end
-
-  def trains_by_type
-    p_train = 0
-    @trains.each.with_index(1) do |t, i|
-     p_train = i if t.train[1] == "people"
-    end
-    puts "Пассажирских: #{p_train}, Грузовых: #{@trains.size - p_train} "
   end
 
 end
@@ -38,68 +31,59 @@ class Route
     @stations = [first_st, last_st]
   end
 
-  def add_st(station)
+  def add_station(station)
     @stations.insert(-2, station)
   end
 
-  def del_st(station)
+  def delete_station(station)
     if station != @stations.last && station != @stations.first
       @stations.delete(station)
     end
-  end
-
-  def show
-    @stations.each.with_index(1){|st, i| puts "#{i}. #{st.name}"}
   end
 
 end
 
 
 class Train
-  attr_reader :train
+  attr_reader :speed, :cars, :type, :station
 
   def initialize(id, type, cars)
-    @train = [id, type, cars.to_i]
     @speed = 0
+    @id = id
+    @type = type
+    @cars = cars.to_i
   end
 
   def speed_up(num)
-    @speed += num
+    @speed += num.abs
   end
 
-  def current_speed
-    puts "Скорость поезда #{@speed}"
-  end
-  
   def brake
     @speed = 0
   end
 
-  def cars
-    puts "Вагонов: #{@train[2]}"
-  end
-
   def add_car
-    @train[2] += 1 if @speed == 0
+    @cars += 1 if @speed == 0 
   end 
 
-  def del_car
-    @train[2] -= 1 if @speed == 0
+  def delete_car
+    @cars -= 1 if @speed == 0 && @cars >= 0
   end
+  
 
   def add_route(route)
     @route = route
-    @station = @route.stations[0]
+    @current = 0
+    @station = @route.stations[@current]
     @station.arrive(self)
   end
 
   def motion(direction)
     @station.departure(self)
-    current = @route.stations.index(@station)
     if direction == "forward"
-      @station = @route.stations[current + 1]
+      @station = @route.stations[@current += 1]
     else
-      @station = @route.stations[current - 1]
+      @station = @route.stations[@current -= 1]
     end
     @station.arrive(self)
   end
@@ -113,14 +97,14 @@ class Train
   end
   
   def current_st
-    puts @station.name
+    @station
   end
 
   def next_st
-    puts @route.stations[@route.stations.index(@station) + 1].name
+    @route.stations[@current + 1] if @station != @route.stations.last
   end
 
   def prev_st
-    puts @route.stations[@route.stations.index(@station) - 1].name
+    @route.stations[@current - 1] if @station != @route.stations.first
   end
 end
