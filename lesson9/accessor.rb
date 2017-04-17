5a2a1e01d32ee928029bdc4d43c7e328418c603a
Pavel Puzin <puzinok@gmail.com>
2017-04-18 02:34:07 +0300

    немного поправил код

--- lesson9/accessor.rb
+++ lesson9/accessor.rb
@@ -5,17 +5,17 @@ module Accessors
 
   module ClassMethods
     def attr_accessor_with_history(*names)
-      @names_list = {}
       names.each do |name|
         var_name = "@#{name}".to_sym
         define_method(name) { instance_variable_get(var_name) }
 
         define_method("#{name}=".to_sym) do |value|
           instance_variable_set(var_name, value)
-          (@names_list[name.to_sym] ||= []) << value
+          @names_list ||= {}
+          (@names_list[var_name] ||= []) << value 
         end
 
-        define_method("#{name}_history") { @names_list[name.to_sym] }
+        define_method("#{name}_history") { @names_list[var_name] }
       end
     end
 
